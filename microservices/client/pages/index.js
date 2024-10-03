9a79f727bdfa8c30312d1609925004dfd390c73f
Abraham Arreola <abrahamarrolab198@gmail.com>
2024-10-03 00:37:26 -0600

    add landing page with getInitialProps code

--- microservices/client/pages/index.js
+++ microservices/client/pages/index.js
@@ -1,3 +1,43 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+// LandingPage.getInitialProps = async () => {
+//   if (typeof window === 'undefined') {
+//     // we are on the server!
+//     // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+//     const { data } = await axios.get(
+//       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+//       {
+//         headers: {
+//           Host: 'ticketing.dev',
+//         },
+//       }
+//     );
+
+//     return data;
+//   } else {
+//     // we are on the browser!
+//     // requests can be made with a base url of ''
+//     const { data } = await axios.get('/api/users/currentuser');
+
+//     return data;
+//   }
+// };
+
+export const getServerSideProps = async ({ req }) => {
+  const { data } = await axios.get(
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+    {
+      headers: req.headers,
+    }
+  );
+
+  return { props: data };
+};
+
+export default LandingPage;
