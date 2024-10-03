94a170ed4c35fc9a3011d2491de3a9de5bed71ef
Abraham Arreola <abrahamarrolab198@gmail.com>
2024-10-03 00:57:05 -0600

    add reusable api client

---
+++ microservices/client/api/build-client.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // We are on the browser
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export const buildClientV2 = ({ req }) => {
+  return axios.create({
+    baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+    headers: req.headers,
+  });
+};
--- microservices/client/pages/index.js
+++ microservices/client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { buildClient, buildClientV2 } from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -29,13 +30,18 @@ const LandingPage = ({ currentUser }) => {
 //   }
 // };
 
-export const getServerSideProps = async ({ req }) => {
-  const { data } = await axios.get(
-    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-    {
-      headers: req.headers,
-    }
-  );
+// LandingPage.getInitialProps = async (context) => {
+//   const client = buildClient(context);
+
+//   const { data } = await client.get('/api/users/currentuser');
+
+//   return data;
+// };
+
+export const getServerSideProps = async (context) => {
+  const client = buildClientV2(context);
+
+  const { data } = await client.get('/api/users/currentuser');
 
   return { props: data };
 };
